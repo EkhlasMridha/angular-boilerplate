475a64613e7b3d257e97740b575b975b02ed47ca
ekhlasmridha <ekhlas.mridha@vivasoftltd.com>
2024-09-20 00:04:24 +0600

    app theme and environment setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,28 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+declare global {
+  interface Window {
+    ServerConst: typeof environment;
+  }
+  type HTMLElementEvent<T extends HTMLElement> = MouseEvent & {
+    target: T;
+  };
+}
+
+window.ServerConst = window.ServerConst || {};
+
+if (environment.production) {
+  enableProdMode();
+}
+
+if (window) {
+  window.ServerConst = environment;
+}
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
